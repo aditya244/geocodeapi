848f1cf277735d5f94771bcd56671c45d44390e6
Aditya <sinha.aditya244@gmail.com>
2018-07-16 08:30:56 +0530

    commit

---
+++ apiapp/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+import requests
+import openpyxl
+# Create your views here.
+def index(request):
+    if "GET" == request.method:
+         return render(request, 'index.html', {})
+    else:
+        excel_file = request.FILES["excel_file"]
+
+    # you may put validations here to check extension or file size
+
+        wb = openpyxl.load_workbook(excel_file)
+
+    # getting a particular sheet by name out of many sheets
+        worksheet = wb["Sheet1"]
+        print(worksheet)
+
+        excel_data = list()
+        # iterating over the rows and
+        # getting value from each cell in row
+        for row in worksheet.iter_rows():
+            row_data = list()
+            for cell in row:
+                row_data.append(str(cell.value))
+                address = (cell.value)
+                api_key = "AIzaSyDSVDzILhdxP3JuRElxyKfvfhbYRpIEmEs"
+                api_response = requests.get(
+                    'https://maps.googleapis.com/maps/api/geocode/json?address={0}&key={1}'.format(address, api_key))
+                api_response_dict = api_response.json()
+
+                if api_response_dict['status'] == 'OK':
+                    latitude = api_response_dict['results'][0]['geometry']['location']['lat']
+                    longitude = api_response_dict['results'][0]['geometry']['location']['lng']
+
+                row_data.append(str(longitude))
+                row_data.append(str(latitude))
+            excel_data.append(row_data)
+
+    return render(request, 'index.html', {"excel_data": excel_data,'longitude':longitude,'latitude':latitude})
